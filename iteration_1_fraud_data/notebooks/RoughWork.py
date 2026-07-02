db278e195c7af396c1812fcc774a0bf5fb946472
Jaikarthik-codes <mylapurjaikarthik@gmail.com>
2026-07-03 01:21:31 +0530

    Reorganize into iteration 1 and iteration 2 structure

---
+++ iteration_1_fraud_data/notebooks/RoughWork.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('../iteration_1_fraud_data/data/processed/Fraud_Data_cleaned.csv')
+X = df.drop(columns=['class'])
+y = df['class']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+
+rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_model.fit(X_train, y_train)
+
+# Check which features the model found most useful
+feature_importance = pd.Series(rf_model.feature_importances_, index=X.columns)
+print(feature_importance.sort_values(ascending=False))
